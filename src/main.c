064179649bd235882a07899111ed53f2fd125afd
Vugar Aghayev <vugar.aghayev@calven.com>
2025-11-18 12:53:48 +1100

    VL-6 fixing test issue

--- src/main.c
+++ src/main.c
@@ -11,7 +11,6 @@ int main(void)
     // Let sliding_window.c allocate the array and set global `arr` + `capacity`
     int *ar = generate_rand_arr(capacity);
 
-
     // Fill and print that same array
     populate_array(ar);      // uses the pointer from generate_rand_arr
     print_array(ar);
